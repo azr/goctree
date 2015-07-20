d279c950efcb60c4e1f1afcd9d0e8ca66d28d5dd
Adrien Delorme <adrien.delorme@icloud.com>
2015-07-20 15:24:51 +0200

    have GetPointsInsideBox return number of walked nodes and termination status

--- walk.go
+++ walk.go
@@ -10,25 +10,23 @@ const (
 // GetPointsInsideBox is a really simple routine for querying the tree for points
 // within a bounding box defined by min/max points (bmin, bmax)
 // All results are pushed into 'results'
-func (o *Node) GetPointsInsideBox(bmin, bmax Vector3D, fn func(Data) WalkChoice) {
-	o.getPointsInsideBox(bmin, bmax, fn)
-}
-
-func (o *Node) getPointsInsideBox(bmin, bmax Vector3D, fn func(Data) WalkChoice) WalkChoice {
+func (o *Node) GetPointsInsideBox(bmin, bmax Vector3D, fn func(Data) WalkChoice) (int, WalkChoice) {
 	// If we're at a leaf node, just see if the current data point is inside
 	// the query bounding box
 	if o.IsLeafNode() {
 		if o.data != nil {
 			p := o.data.GetPosition()
 			if p[x] > bmax[x] || p[y] > bmax[y] || p[z] > bmax[z] {
-				return ContinueWalking
+				return 1, ContinueWalking
 			}
 			if p[x] < bmin[x] || p[y] < bmin[y] || p[z] < bmin[z] {
-				return ContinueWalking
+				return 1, ContinueWalking
 			}
-			return fn(o.data)
+			return 1, fn(o.data)
 		}
+		return 1, ContinueWalking
 	} else {
+		walked := 1
 		// We're at an interior node of the tree. We will check to see if
 		// the query bounding box lies outside the octants of this node.
 		for i := 0; i < 8; i++ {
@@ -47,11 +45,12 @@ func (o *Node) getPointsInsideBox(bmin, bmax Vector3D, fn func(Data) WalkChoice)
 
 			// At this point, we've determined that this child is intersecting
 			// the query bounding box
-			if o.children[i].getPointsInsideBox(bmin, bmax, fn) == StopWalking {
-				return StopWalking
+			n, c := o.children[i].GetPointsInsideBox(bmin, bmax, fn)
+			walked += n
+			if c == StopWalking {
+				return walked, StopWalking
 			}
 		}
+		return walked, ContinueWalking
 	}
-
-	return ContinueWalking
 }
